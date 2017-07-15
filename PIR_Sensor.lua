2b40991e402757a45ae501670bc7270c67e28842
thierry@moncourriel.eu <thierry>
2017-07-15 22:10:44 +0200

    Ajout de la version LUA
    Montage fonctionnel mais j'ai du enlever le speaker par manque de place.
    Mon bread board est trop petit il faut que j'en achete un autre :'(

---
+++ PIR_Sensor.lua
@@ -0,0 +1,36 @@
+-- traduction de ce projet http://blog.makezine.com/projects/pir-sensor-arduino-alarm/
+-- En gros je passe de arduino/C vers NodeMCU/Lua
+
+ledPin = 3
+inputPin = 4
+-- arret du speaker par manque de place sur la board speakerPin = 2
+lastState = gpio.LOW
+state = 0; -- variable val a l'origine mais etat c'est mieux, state en english
+
+function setup() 
+--  pwm.setup(speakerPin, 100, 512)
+  gpio.mode(ledPin,gpio.OUTPUT)
+  gpio.mode(inputPin,gpio.INPUT)
+  gpio.write(ledPin, gpio.LOW)
+end
+
+function bipbip(state,tmp)
+  if state == gpio.HIGH 
+    then
+ --     pwm.start(speakerPin)
+      gpio.write(ledPin,gpio.HIGH)
+      if (lastState == gpio.LOW) then
+	print "mouvement detecté!"
+	lastState = gpio.HIGH
+      end
+    else
+  --    pwm.stop(speakerPin)
+      gpio.write(ledPin, gpio.LOW)
+      if lastState == gpio.HIGH then
+	print "ça ne bouge plus...C'est vide"
+	lastState = gpio.LOW
+      end
+    end
+  end
+  setup()
+  gpio.trig(inputPin, "both", bipbip)
