77f587986702ede80a744e102e38ca268ebef289
thierry@moncourriel.eu <thierry>
2017-07-19 07:11:00 +0200

    Ajout messages udp et temps
    * J'ai passé le firmware en 2.1.0, a cause du differentiel API
    * J'ai ajouté les modules RTCTIME et SNTP dans le firmware
    * J'ai forké le repot du firmware mais pas mis à jour mon repot
    * J'envoi a présent les msg en udp au lieu d'un print console
    * En plus les messages sont timestampé(à l'heure UTC)

--- src/PIR_Sensor.lua
+++ src/PIR_Sensor.lua
@@ -6,13 +6,27 @@ inputPin = 4
 speakerPin = 2
 lastState = gpio.LOW
 state = 0; -- variable val a l'origine mais etat c'est mieux, state en english
+data_presence = "mouvement detecté!\n"
+data_absence = "ça ne bouge plus...C'est vide\n"
+ip_cible = "192.168.1.28"
+port_cible = 5000
 
 function setup() 
   pwm.setup(speakerPin, 100, 512)
   gpio.mode(ledPin,gpio.OUTPUT)
   gpio.mode(inputPin,gpio.INPUT)
   gpio.write(ledPin, gpio.LOW)
+  -- creation d'un socket udp
+  udpSocket = net.createUDPSocket()
+  -- synchro du temps
+  sntp.sync()
 end
+
+function what_time() 
+  tm = rtctime.epoch2cal(rtctime.get())
+  return string.format("%04d/%02d/%02d %02d:%02d:%02d", tm["year"], tm["mon"], tm["day"], tm["hour"], tm["min"], tm["sec"])
+end
+
 
 function bipbip(state,tmp)
   if state == gpio.HIGH 
@@ -20,14 +34,16 @@ function bipbip(state,tmp)
       pwm.start(speakerPin)
       gpio.write(ledPin,gpio.HIGH)
       if (lastState == gpio.LOW) then
-	print "mouvement detecté!"
+	hr = what_time()
+	udpSocket:send(port_cible,ip_cible,hr..";"..data_presence)
 	lastState = gpio.HIGH
       end
     else
       pwm.stop(speakerPin)
       gpio.write(ledPin, gpio.LOW)
       if lastState == gpio.HIGH then
-	print "ça ne bouge plus...C'est vide"
+	hr = what_time()
+	udpSocket:send(port_cible,ip_cible,hr..";"..data_absence)
 	lastState = gpio.LOW
       end
     end
