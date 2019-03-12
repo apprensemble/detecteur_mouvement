e9b0b647f41becfa7a37d7ecb0fe1332b953eca1
thierry <thierry@apprensemble.link>
2019-03-12 08:18:37 +0100

    evolution API du SDK nodemcu

--- src/ecran/messages_udp.lua
+++ src/ecran/messages_udp.lua
@@ -16,6 +16,7 @@
 -- -- Variables 
 sda = 1 -- SDA Pin
 scl = 2 -- SCL Pin
+id = 0
 entete = " "
 destinataire = "     @Grillon"
 message = "1234567890123456789012"
@@ -49,11 +50,11 @@ end
 
 function init_OLED(sda,scl) --Set up the u8glib lib
      sla = 0x3C
-     i2c.setup(0, sda, scl, i2c.SLOW)
-     disp = u8g.ssd1306_128x64_i2c(sla)
-     disp:setFont(u8g.font_6x10) -- about 6 lines & 21 char by line
+     i2c.setup(id, sda, scl, i2c.SLOW)
+     -- disp = u8g2.ssd1306_128x64_i2c(sla)
+		 disp = u8g2.ssd1306_i2c_128x64_noname(id, sla)
+     disp:setFont(u8g2.font_6x10_tf) -- about 6 lines & 21 char by line
      disp:setFontRefHeightExtendedText()
-     disp:setDefaultForegroundColor()
      disp:setFontPosTop()
      --disp:setRot180()           -- Rotate Display if needed
 end
@@ -64,9 +65,8 @@ function what_time()
 end
 
 function write_OLED() -- Write Display
-   disp:firstPage()
-   repeat
      --disp:drawFrame(2,2,126,62)
+		 disp:clearBuffer()
      disp:drawStr(5, 0, entete)
      -- disp:drawStr(5, 7, str1)
      -- disp:drawStr(40, 30,  string.format("%02d:%02d:%02d",h,m,s))
@@ -74,10 +74,10 @@ function write_OLED() -- Write Display
      disp:drawStr(0, 15, message)
      disp:drawStr(0, 35, date)
      disp:drawStr(0, 50, provenance)
+		 disp:sendBuffer()
      --disp:drawXBM(0, 0, 128, 64, xbm_data)
      --disp:drawXBM( 0, 20, 38, 24, xbm_data )
      --disp:drawCircle(18, 47, 14)
-   until disp:nextPage() == false
 end
 
 function loadXbm() 
